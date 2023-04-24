4e5b6f81ec42652ba88ec6ed71221ad880f8a14a
Max MacGillivray <mary.macgillivray@gmail.com>
2023-04-24 00:13:55 -0700

    Testing is currently at 100% for facades and services

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -7,19 +7,24 @@ RSpec.describe WeatherFacade do
       
       expect(weather.class).to eq(Weather)
       expect(weather.id).to eq(nil)
-      # binding.pry
+
       expect(weather.current_weather.class).to eq(Hash)
       expect(weather.current_weather.keys).to eq([:last_updated, :temperature, :feels_like, :humidity, :uvi, :visibility, :condition, :icon])
+      
+      expect(weather.current_weather.keys).not_to include(:wind_mph)
       
       expect(weather.hourly_weather.class).to eq(Array)
       expect(weather.hourly_weather.first.class).to eq(Hash)
       expect(weather.hourly_weather.count).to eq(24)
       expect(weather.hourly_weather.first.keys).to eq([:time, :temperature, :conditions, :icon])
+      expect(weather.hourly_weather.first.keys).not_to include(:windchill_f)
       
       expect(weather.daily_weather.class).to eq(Array)
       expect(weather.daily_weather.first.class).to eq(Hash)
       expect(weather.daily_weather.count).to eq(5)
       expect(weather.daily_weather.first.keys).to eq([:date, :sunrise, :sunset, :max_temp, :min_temp, :condition, :icon])
+      
+      expect(weather.daily_weather.first.keys).not_to include(:moon_phase)
     end
     
     it 'returns a set of #coords' do
