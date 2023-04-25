5189fefe4e70c308149e09f856b7ce9cc82f03df
Max MacGillivray <mary.macgillivray@gmail.com>
2023-04-25 12:53:46 -0700

    Merged in main to incorporate some changes I made last night as well as changing route back to index from show

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ForecastController < ApplicationController
-  def show
+  def index
     # binding.pry
     forecast = ForecastFacade.forecast(params[:location])
     # binding.pry
--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe 'Forecast API', type: :request do
   it 'pulls the forecast for the location' do
-    VCR.use_cassette('forecast_controller/get_forecast_show') do
-      get '/api/v1/forecast/cincinnati,oh'
-      
+    VCR.use_cassette('forecast_controller/get_forecast_index') do
+      get "/api/v1/forecast?location=cincinnati,oh"
+      # binding.pry
       expect(response).to be_successful
       
       fc = JSON.parse(response.body, symbolize_names:true)
