511fbdb59af716c7c9bdd8cccc78e3154e149ad9
Max MacGillivray <mary.macgillivray@gmail.com>
2023-04-25 08:44:31 -0700

    Created initial facade methods and set up testing, WIP

--- app/facades/activity_facade.rb
+++ app/facades/activity_facade.rb
@@ -1,13 +1,26 @@
 class ActivityFacade
-  def temp # >= 60 F(destination)
-    forecast = ForecastFacade.forecast(params[:destination]).current_weather
+  def temp_over_60(destination)
+    # #if the temperature is >= 60 F, include one recreational activity.
+    forecast = ForecastFacade.forecast(destination).current_weather
+    
+    activity_one = ActivityService.get_activity_by_type('relaxation')
+    activity_two = ActivityService.get_activity_by_type('recreational')
+    binding.pry
   end
   
-  def temp # >= 50 F and < 60 F
-    forecast = ForecastFacade.forecast(params[:destination]).current_weather
+  def temp_between_50_60(destination)
+    # #if the temperature is >= 50 F and < 60 F, include one busywork activity.
+    forecast = ForecastFacade.forecast(destination).current_weather
+    activity_one = ActivityService.get_activity_by_type('relaxation')
+    activity_two = ActivityService.get_activity_by_type('busywork')
+    binding.pry
   end
   
-  def temp # < 50 F
-    forecast = ForecastFacade.forecast(params[:destination]).current_weather
+  def temp_under_50 # < 50 F(destination)
+    # #if the temperature is < 50 F, include one cooking activity.
+    forecast = ForecastFacade.forecast(destination).current_weather
+    activity_one = ActivityService.get_activity_by_type('relaxation')
+    activity_two = ActivityService.get_activity_by_type('cooking')
+    binding.pry
   end
 end
--- spec/facades/activity_facade_spec.rb
+++ spec/facades/activity_facade_spec.rb
@@ -1,14 +1,32 @@
-# require 'rails_helper'
-# 
-# RSpec.describe ActivityFacade do
-#   context 'methods' do
-#     it '#method_name' do
-#       VCR.use_cassette('activity_facade/#name of method') do
-# 
-# 
-# 
-#       VCR.eject_cassette
-#       end
-#     end
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe ActivityFacade do
+  context 'methods' do
+    it '#temp_over_60' do
+      # VCR.use_cassette('activity_facade/temp_over_60') do
+
+
+
+      # VCR.eject_cassette
+      # end
+    end
+    
+    it '#temp_between_50_60' do
+      # VCR.use_cassette('activity_facade/temp_between_50_60') do
+
+
+
+      # VCR.eject_cassette
+      # end
+    end
+    
+    it '#temp_under_50' do
+      # VCR.use_cassette('activity_facade/temp_under_50') do
+
+
+
+      # VCR.eject_cassette
+      # end
+    end
+  end
+end
