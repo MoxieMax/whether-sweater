f1d70c7a45573ac35fa3d439e4aacb1ffc0b4235
Max MacGillivray <mary.macgillivray@gmail.com>
2023-04-25 14:49:43 -0700

    Added api_key to user serializer, tested user.send(:generate_api_key) to ensure there's an api key being generated.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
+  before_create :generate_api_key
   validates :email, presence: true, 
                     uniqueness: true
   validates :password, presence: true
   validates :password_confirmation, presence: { on: :create }
   
-  before_create :generate_api_key
+  validates :api_key, presence: true,
+                      uniqueness: true
+  # validates :api_key, presence: true
   
   private
     def generate_api_key
