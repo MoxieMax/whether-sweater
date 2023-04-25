5ae8a2f4a7cb1db16fcd6f76477a03efe250ddc0
Max MacGillivray <mary.macgillivray@gmail.com>
2023-04-25 00:03:27 -0700

    Added VCR to all existing tests.

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -3,36 +3,43 @@ require 'rails_helper'
 RSpec.describe ForecastFacade do
   context 'methods' do
     it 'returns a #forecast and creates a poro' do
-      weather = ForecastFacade.forecast("Cincinnati, OH")
-      
-      expect(weather.class).to eq(Forecast)
-      expect(weather.id).to eq(nil)
-      
-      expect(weather.current_weather.class).to eq(Hash)
-      expect(weather.current_weather.keys).to eq([:last_updated, :temperature, :feels_like, :humidity, :uvi, :visibility, :condition, :icon])
-      
-      expect(weather.current_weather.keys).not_to include(:wind_mph)
-      
-      expect(weather.hourly_weather.class).to eq(Array)
-      expect(weather.hourly_weather.first.class).to eq(Hash)
-      expect(weather.hourly_weather.count).to eq(24)
-      expect(weather.hourly_weather.first.keys).to eq([:time, :temperature, :conditions, :icon])
-      expect(weather.hourly_weather.first.keys).not_to include(:windchill_f)
-      
-      expect(weather.daily_weather.class).to eq(Array)
-      expect(weather.daily_weather.first.class).to eq(Hash)
-      expect(weather.daily_weather.count).to eq(5)
-      expect(weather.daily_weather.first.keys).to eq([:date, :sunrise, :sunset, :max_temp, :min_temp, :condition, :icon])
-      
-      expect(weather.daily_weather.first.keys).not_to include(:moon_phase)
+      VCR.use_cassette('forecast_facade/get_forecast') do
+        weather = ForecastFacade.forecast("Cincinnati, OH")
+        
+        expect(weather.class).to eq(Forecast)
+        expect(weather.id).to eq(nil)
+        
+        expect(weather.current_weather.class).to eq(Hash)
+        expect(weather.current_weather.keys).to eq([:last_updated, :temperature, :feels_like, :humidity, :uvi, :visibility, :condition, :icon])
+        
+        expect(weather.current_weather.keys).not_to include(:wind_mph)
+        
+        expect(weather.hourly_weather.class).to eq(Array)
+        expect(weather.hourly_weather.first.class).to eq(Hash)
+        expect(weather.hourly_weather.count).to eq(24)
+        expect(weather.hourly_weather.first.keys).to eq([:time, :temperature, :conditions, :icon])
+        expect(weather.hourly_weather.first.keys).not_to include(:windchill_f)
+        
+        expect(weather.daily_weather.class).to eq(Array)
+        expect(weather.daily_weather.first.class).to eq(Hash)
+        expect(weather.daily_weather.count).to eq(5)
+        expect(weather.daily_weather.first.keys).to eq([:date, :sunrise, :sunset, :max_temp, :min_temp, :condition, :icon])
+        
+        expect(weather.daily_weather.first.keys).not_to include(:moon_phase)
+        VCR.eject_cassette
+      end
     end
     
     it 'returns a set of #coords' do
-      coords = ForecastFacade.coords("Cincinnati, OH")
-      
-      expect(coords).to be_a(Hash)
-      expect(coords[:lat]).to eq(39.10713)
-      expect(coords[:lng]).to eq(-84.50413)
+      VCR.use_cassette('forecast_facade/coords') do
+        
+        coords = ForecastFacade.coords("Cincinnati, OH")
+        
+        expect(coords).to be_a(Hash)
+        expect(coords[:lat]).to eq(39.10713)
+        expect(coords[:lng]).to eq(-84.50413)
+        VCR.eject_cassette
+      end
     end
   end
 end
--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -1,32 +1,35 @@
 require 'rails_helper'
 
-describe 'Forecast API' do
+describe 'Forecast API', type: :request do
   it 'pulls the forecast for the location' do
-    get '/api/v1/forecast?location=cincinnati,oh'
-    
-    expect(response).to be_successful
-    
-    fc = JSON.parse(response.body, symbolize_names:true)
-    
-    expect(fc[:data]).to be_a(Hash)
-    expect(fc[:id]).to be nil
-    expect(fc[:data][:type]).to eq('forecast')
-    expect(fc[:data][:attributes]).to be_a(Hash)
-    
-    expect(fc[:data][:attributes][:current_weather]).to be_a(Hash)
-    expect(fc[:data][:attributes][:current_weather].length).to eq(8)
-    expect(fc[:data][:attributes][:current_weather]).to include(:last_updated, :temperature, :feels_like, :humidity, :uvi, :visibility, :condition, :icon)
-    
-    expect(fc[:data][:attributes][:daily_weather]).to be_an(Array)
-    expect(fc[:data][:attributes][:daily_weather].length).to eq(5)
-    expect(fc[:data][:attributes][:daily_weather].first).to be_a Hash
-    expect(fc[:data][:attributes][:daily_weather].first.length).to eq(7)
-    expect(fc[:data][:attributes][:daily_weather].first).to include(:date, :sunrise, :sunset, :max_temp, :min_temp, :condition, :icon)
-    
-    expect(fc[:data][:attributes][:hourly_weather]).to be_an(Array)
-    expect(fc[:data][:attributes][:hourly_weather].length).to eq(24)
-    expect(fc[:data][:attributes][:hourly_weather].first).to be_a(Hash)
-    expect(fc[:data][:attributes][:hourly_weather].first.length).to eq(4)
-    expect(fc[:data][:attributes][:hourly_weather].first.keys).to eq([:time, :temperature, :conditions, :icon])
+    VCR.use_cassette('forecast_controller/get_forecast') do
+      get '/api/v1/forecast?location=cincinnati,oh'
+      
+      expect(response).to be_successful
+      
+      fc = JSON.parse(response.body, symbolize_names:true)
+      
+      expect(fc[:data]).to be_a(Hash)
+      expect(fc[:id]).to be nil
+      expect(fc[:data][:type]).to eq('forecast')
+      expect(fc[:data][:attributes]).to be_a(Hash)
+      
+      expect(fc[:data][:attributes][:current_weather]).to be_a(Hash)
+      expect(fc[:data][:attributes][:current_weather].length).to eq(8)
+      expect(fc[:data][:attributes][:current_weather]).to include(:last_updated, :temperature, :feels_like, :humidity, :uvi, :visibility, :condition, :icon)
+      
+      expect(fc[:data][:attributes][:daily_weather]).to be_an(Array)
+      expect(fc[:data][:attributes][:daily_weather].length).to eq(5)
+      expect(fc[:data][:attributes][:daily_weather].first).to be_a Hash
+      expect(fc[:data][:attributes][:daily_weather].first.length).to eq(7)
+      expect(fc[:data][:attributes][:daily_weather].first).to include(:date, :sunrise, :sunset, :max_temp, :min_temp, :condition, :icon)
+      
+      expect(fc[:data][:attributes][:hourly_weather]).to be_an(Array)
+      expect(fc[:data][:attributes][:hourly_weather].length).to eq(24)
+      expect(fc[:data][:attributes][:hourly_weather].first).to be_a(Hash)
+      expect(fc[:data][:attributes][:hourly_weather].first.length).to eq(4)
+      expect(fc[:data][:attributes][:hourly_weather].first.keys).to eq([:time, :temperature, :conditions, :icon])
+      VCR.eject_cassette
+    end
   end
 end
