70b5d77905751036e15ea1714939e6ae52b7d567
Max MacGillivray <mary.macgillivray@gmail.com>
2023-04-25 15:08:49 -0700

    Working through testing for user create via post with route

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -1,59 +1,92 @@
 require 'rails_helper'
 
-describe UserController, type: :request do
+RSpec.describe "UserController", type: :request do
+  let(:correct_params) do {
+                            email: "cavlife@ninth.net",
+                            password: "FrontlineFifth",
+                            password_confirmation: "FrontlineFifth"
+                          }
+                       end
+  
+  let(:unmatch_params) do {
+                            email: "lyctorish@first.mil",
+                            password: "Coronabeth",
+                            password_confirmation: "Corona"
+                          }
+                       end
+  
+  let(:missing_param1) do {
+                            email: "",
+                            password: "password",
+                            password_confirmation: "password"
+                          }
+                       end
+  
+  let(:missing_param2) do {
+                            email: "AbigailPent@fifth.edu",
+                            password: "",
+                            password_confirmation: ""
+                          }
+                       end
+  
+  
   describe "when successful" do
     it '#creates new user' do
-      VCR.use_cassette('users_controller/create') do
-        post "http://localhost:3000/api/v1/users", params:
+      User.destroy_all
+      
+      VCR.use_cassette('users_controller/create/success') do
+        post "/api/v1/users", params: correct_params.to_json, headers: { 'CONTENT_TYPE' => 'application/json' }
+        binding.pry
+        expect(response).to be_successful
         binding.pry
       VCR.eject_cassette
       end
     end
   end
   
-  describe "when unsuccessful returns errors" do
-    it "for existing user" do
-    #   VCR.use_cassette('users_controller/create') do
-    #     post "http://localhost:3000/api/v1/users", params:
-    #     binding.pry
-    #   VCR.eject_cassette
-    #   end
-    end
-    
-    it "for unmatched passwords" do
-    #   VCR.use_cassette('users_controller/create') do
-    #     post "http://localhost:3000/api/v1/users", params:
-    #     binding.pry
-    #   VCR.eject_cassette
-    #   end
-    end
-    
-    it "for missing information" do
-    #   VCR.use_cassette('users_controller/create') do
-    #     post "http://localhost:3000/api/v1/users", params:
-    #     binding.pry
-    #   VCR.eject_cassette
-    #   end
-    end
-  end
-  
-  describe "when unsuccessful" do
-    it '#creates new user' do
-      VCR.use_cassette('users_controller/create') do
-        post "http://localhost:3000/api/v1/users", params:
-        binding.pry
-      VCR.eject_cassette
-      end
-    end
-  end
-  
-  describe "when unsuccessful" do
-    # it '#creates new user' do
-    #   VCR.use_cassette('users_controller/create') do
-    #     post "http://localhost:3000/api/v1/users", params:
-    #     binding.pry
-    #   VCR.eject_cassette
-    #   end
-    # end
-  end
+  # describe "when unsuccessful returns errors" do
+  #   it "for existing user" do
+  #   #   VCR.use_cassette('users_controller/create/fail_exists') do
+  #   #     post "http://localhost:3000/api/v1/users", params:
+  #   #     binding.pry
+  #   #   VCR.eject_cassette
+  #   #   end
+  #   end
+  # 
+  #   it "for unmatched passwords" do
+  #   #   VCR.use_cassette('users_controller/create/fail_passwords') do
+  #   #     post "http://localhost:3000/api/v1/users", params:
+  #   #     binding.pry
+  #   #   VCR.eject_cassette
+  #   #   end
+  #   end
+  # 
+  #   it "for missing information" do
+  #   #   VCR.use_cassette('users_controller/create/fail_missing') do
+  #   #     post "http://localhost:3000/api/v1/users", params:
+  #   #     binding.pry
+  #   #   VCR.eject_cassette
+  #   #   end
+  #   end
+  # end
+  # 
+  # describe "when unsuccessful" do
+  #   it '#creates new user' do
+  #     VCR.use_cassette('users_controller/create') do
+  #       post "http://localhost:3000/api/v1/users", params:
+  #       binding.pry
+  #     VCR.eject_cassette
+  #     end
+  #   end
+  # end
+  # 
+  # describe "when unsuccessful" do
+  #   # it '#creates new user' do
+  #   #   VCR.use_cassette('users_controller/create') do
+  #   #     post "http://localhost:3000/api/v1/users", params:
+  #   #     binding.pry
+  #   #   VCR.eject_cassette
+  #   #   end
+  #   # end
+  # end
 end
