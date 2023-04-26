b08be926b90f434fc539bd5029dcc52476ea5fd6
Max MacGillivray <mary.macgillivray@gmail.com>
2023-04-26 08:21:49 -0700

    Updated a cassette, updated some testing.

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -55,7 +55,7 @@ RSpec.describe 'Road Trip API', type: :request do
           expect(trip[:data][:attributes][:travel_time]).to eq("04:42:32")
           
           expect(trip[:data][:attributes][:weather_at_eta].keys).to eq([:datetime, :temperature, :condition])
-          expect(trip[:data][:attributes][:weather_at_eta]).to eq({:condition=>"Sunny", :datetime=>"2023-04-26 12:00", :temperature=>49.8})
+          expect(trip[:data][:attributes][:weather_at_eta]).to eq({:condition=>"Sunny", :datetime=>"2023-04-26 13:00", :temperature=>48.2})
           # expect(trip[:data][:attributes][:weather_at_eta].class).to eq(Hash)
           #needs to change to match json response
           VCR.eject_cassette
@@ -130,35 +130,29 @@ RSpec.describe 'Road Trip API', type: :request do
         end
       end
       
-      # it "does not #create a trip" do
-      #   VCR.use_cassette('road_trip_api/impossible_road_trip') do
-      #     trip_params = {
-      #                     origin: 'new york,ny',
-      #                     destination: 'london,uk',
-      #                     api_key: "string"
-      #                   }
-      #     post '/api/v1/road_trip', params: trip_params.to_json, headers: headers
-      # 
-      #     expect(response).to be_successful
-      #     trip = JSON.parse(response.body, symbolize_names: true).deep_symbolize_keys
-      #     binding.pry
-      #     expect(trip.class).to eq(Hash)
-      # 
-      #     # expect(trip[:data].class).to eq(Hash)
-      #     # expect(trip[:data].keys).to eq([:id, :type, :attributes])
-      #     # expect(trip[:data][:id]).to eq(nil)
-      #     # expect(trip[:data][:type]).to eq("road_trip")
-      #     # 
-      #     # expect(trip[:data][:attributes].class).to eq(Hash)
-      #     # expect(trip[:data][:attributes].keys).to eq([:start_city, :end_city, :travel_time, :weather_at_eta])
-      #     # expect(trip[:data][:attributes][:start_city]).to eq("new york,ny")
-      #     # expect(trip[:data][:attributes][:end_city]).to eq("london,uk")
-      #     # expect(trip[:data][:attributes][:travel_time]).to eq("impossible")
-      #     # expect(trip[:data][:attributes][:weather_at_eta]).to eq({})
-      # 
-      #     VCR.eject_cassette
-      #   end
-      # end
+      it "does not #create a trip" do
+        VCR.use_cassette('road_trip_api/unauthorized_road_trip') do
+          trip_params = {
+                          origin: 'cincinnati,oh',
+                          destination: 'chicago,il',
+                          api_key: "string"
+                        }
+          post '/api/v1/road_trip', params: trip_params.to_json, headers: headers
+      
+          expect(response).to be_successful
+          
+          trip = JSON.parse(response.body, symbolize_names: true).deep_symbolize_keys
+          
+          expect(trip.class).to eq(Hash)
+          
+          expect(trip.keys).to eq([:error, :status])
+          
+          expect(trip[:error]).to eq("Unauthorized")
+          expect(trip[:status]).to eq(401)
+      
+          VCR.eject_cassette
+        end
+      end
     end
   end
 end
