e7e0d51e71c8bbcf560563c77e32205be59092d1
Max MacGillivray <mary.macgillivray@gmail.com>
2023-04-25 17:16:13 -0700

    Removed pry and commented code

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Forecast API', type: :request do
   it 'pulls the forecast for the location' do
     VCR.use_cassette('forecast_controller/get_forecast_index') do
       get "/api/v1/forecast?location=cincinnati,oh"
-      # binding.pry
+      
       expect(response).to be_successful
       
       fc = JSON.parse(response.body, symbolize_names:true)
--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Sessions API', type: :request do
-  context '#create' do
-    VCR.use_cassette('forecast_controller/get_forecast_index') do
-      get "/api/v1/forecast?location=cincinnati,oh"
-      # binding.pry
-      expect(response).to be_successful
+  context '#create Session' do
+    it 'when given correct params' do
+      VCR.use_cassette('sessions_controller/create_success') do
+        # get "/api/v1/forecast?location=cincinnati,oh"
+        # binding.pry
+        expect(response).to be_successful
+      end
     end
+    
+    # it 'when given incorrect params' do
+    #   VCR.use_cassette('sessions_controller/create_failure') do
+    #     # get "/api/v1/forecast?location=cincinnati,oh"
+    #     # binding.pry
+    #     expect(response).to be_successful
+    #   end
+    # end
   end
 end
